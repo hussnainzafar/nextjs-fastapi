6530aa2b807121011ef8a6bbb12a63f5c4e93197
hussnain-anablog <hussnain@anablock.com>
2025-02-21 19:44:34 +0500

    i am learning the version controll

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
+            ? "http://127.0.0.1:8000/api_new/py/openapi.json"
             : "/api/py/openapi.json",
       },
     ];
