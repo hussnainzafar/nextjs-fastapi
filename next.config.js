eb0a0270562b178a534da59a9420df0fa54b8460
hussnain-anablog <hussnain@anablock.com>
2025-02-24 17:53:22 +0500

    get env veriable

--- next.config.js
+++ next.config.js
@@ -7,42 +7,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/py/:path*",
-      },
-      {
-        source: "/crawl",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/crawl"
-            : "/api/py/crawl",
-      },
-      {
-        source: "/vector-store/info",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/vector-store/info"
-            : "/api/py/vector-store/info",
-      },
-      {
-        source: "/vector-store/search",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/vector-store/search"
-            : "/api/py/vector-store/search",
-      },
-      {
-        source: "/status/:taskId",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/status/:taskId"
-            : "/api/py/status/:taskId",
-      },
-      {
-        source: "/ws/:taskId",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/ws/:taskId"
-            : "/api/py/ws/:taskId",
+            : "/api/",
       },
       {
         source: "/docs",
